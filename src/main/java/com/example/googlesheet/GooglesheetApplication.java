1f59b363a219838a5a84e9d75d5fa24c8f2d5b0d
David Rolón <rolondavid@outlook.com>
2024-10-28 00:40:19 -0300

    first commit

---
+++ src/main/java/com/example/googlesheet/GooglesheetApplication.java
@@ -0,0 +1,13 @@
+package com.example.googlesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglesheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglesheetApplication.class, args);
+	}
+
+}
